b4cad1e62934a6b81edfbaab90c3006348430857
Lars Westermann <lars-westermann@live.de>
2019-08-19 14:29:46 +0200

    Improve spelling in c3-c6

--- images/Rule6BrosEvent.kt
+++ images/Rule6BrosEvent.kt
@@ -1,24 +1,43 @@
 fun Context.setupRule6() {
-
-    match<BpmnTask, Event> { bpmn, bros ->
-        matchStrings(bpmn.element.name, bros.element.desc)
-    }
-    match<BpmnTask, ReturnEvent> { bpmn, bros ->
-        matchStrings(bpmn.element.name, bros.element.desc)
-    }
-
-    verifyBros<Event>("Rule 6 - BrosEvent") { bros ->
-        for (element in bros.matchingElements) {
-            val match = element.element as? BpmnElement ?: continue
-            return@verifyBros Result.match("...", bpmn = element)
+    match<BpmnGateway, BrosEvent> { bpmn, bros ->
+        bpmn.relations<BpmnSequenceFlow>().any { flow ->
+            flow.relation.name.isNotBlank() &&
+                    matchStrings(
+                        flow.relation.name, 
+                        bros.element.desc
+                    )
         }
-        Result.error("...")
+    }
+    match<BpmnGateway, BrosReturnEvent> { bpmn, bros ->
+        bpmn.relations<BpmnSequenceFlow>().any { flow ->
+            flow.relation.name.isNotBlank() &&
+                    matchStrings(
+                        flow.relation.name, 
+                        bros.element.desc
+                    )
+        }
     }
 
-    verifyBros<ReturnEvent>("Rule 6 - BrosReturnEvent") { bros ->
-        for (element in bros.matchingElements) {
-            val match = element.element as? BpmnElement ?: continue
-            return@verifyBros Result.match("...", bpmn = element)
+    match<BpmnTask, BrosEvent> { bpmn, bros ->
+        matchStrings(bpmn.element.name, bros.element.desc)
+    }
+    match<BpmnTask, BrosReturnEvent> { bpmn, bros ->
+        matchStrings(bpmn.element.name, bros.element.desc)
+    }
+
+    verifyBros<BrosEvent> { bros ->
+        for (bpmn in bros.matchingElements) {
+            if (bpmn.checkType<BpmnElement>()) {
+                return@verifyBros Result.match("...", bpmn = bpmn)
+            }
+        }
+        Result.error(".")
+    }
+    verifyBros<BrosReturnEvent> { bros ->
+        for (bpmn in bros.matchingElements) {
+            if (bpmn.checkType<BpmnElement>()) {
+                return@verifyBros Result.match("...", bpmn = bpmn)
+            }
         }
         Result.error("...")
     }
